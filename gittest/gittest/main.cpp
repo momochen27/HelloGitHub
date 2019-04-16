487aa454a3ce94933e9b29fcfd1b09707417f463
陈超群 <705463042@qq.com>
2019-04-16 20:57:42 +0800

    just have a try

---
+++ gittest/gittest/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  gittest
+//
+//  Created by 陈超群 on 2019/4/16.
+//  Copyright © 2019年 陈超群. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    std::cout << "Still sad today!"
+    return 0;
+}
